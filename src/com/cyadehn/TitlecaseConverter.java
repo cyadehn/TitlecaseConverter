8fca06e5b7e3214ec7dcdaa7963f50b5f20113e3
Christopher Dehner <christopher.dehner@outlook.com>
2023-08-15 13:48:39 -0400

    fix: always convert first word to uppercase

--- src/com/cyadehn/TitlecaseConverter.java
+++ src/com/cyadehn/TitlecaseConverter.java
@@ -4,14 +4,16 @@ public class TitlecaseConverter {
     public static String convertToTitleCase(String input) {
         StringBuilder builder = new StringBuilder();
 
+        int i = 0;
         for (String word : input.split("\\s")) {
-            if (word.length() < 4) {
+            if (word.length() < 4 && !(i < 1) ) {
                 builder.append(word.toLowerCase());
             } else {
                 builder.append(word.substring(0,1).toUpperCase());
                 builder.append(word.substring(1).toLowerCase());
             }
             builder.append(" ");
+            i ++;
         }
 
         return builder.toString();
