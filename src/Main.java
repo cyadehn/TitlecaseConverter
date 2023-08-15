1114c4b5e698d2f0a7c6ec802bdedf8063987930
Christopher Dehner <christopher.dehner@outlook.com>
2023-08-15 13:32:09 -0400

    feat: add gui for converting text to title case

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+   public static void main(String... args) {
+       JFrame application = createGUI();
+       application.setVisible(true);
+   }
+
+   private static JFrame createGUI() {
+       JTextField input = new JTextField();
+       input.setPreferredSize(new Dimension(300,40));
+
+       JButton convertButton = new JButton("Convert");
+
+       JLabel output = new JLabel();
+       output.setPreferredSize(new Dimension(300,40));
+
+       convertButton.addActionListener(event -> {
+           output.setText(input.getText());
+       });
+
+       JFrame gui = new JFrame("Title case converter");
+       gui.setLayout(new FlowLayout());
+       gui.add(input);
+       gui.add(convertButton);
+       gui.add(output);
+       gui.pack();
+       gui.setLocationRelativeTo(null); // Centering the screen
+
+       return gui;
+   }
+}
