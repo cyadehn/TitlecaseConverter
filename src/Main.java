70e3f803889e1cead58ad0e7bee5d19f1a0b5443
Christopher Dehner <christopher.dehner@outlook.com>
2023-08-15 13:34:05 -0400

    feat: exit on close

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Main {
    public static void main(String... args) {
        JFrame application = createGUI();
+       application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
        application.setVisible(true);
    }
 
